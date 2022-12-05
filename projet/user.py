f446670d3831561a9a9d34c640ee44c5aa3c16ae
bricekouetcheu <brcronld@gmail.com>
2022-12-05 15:34:14 +0100

    Initialisation des classes

---
+++ projet/user.py
@@ -0,0 +1,18 @@
+class Users:
+    """
+    class users represent a co-house' member
+    """
+
+    def __init__(self, Name, Password):
+        """ this function allows us to create a user based on his name and his password
+            PRE: receive name and password
+            POST: an user object
+
+        """
+        self._name = Name
+        self._password = Password
+
+
+
+
+    pass
