685297a8c0da9591ae5c544c471a7c91d61a9cfc
Mosbah-El-Ajmi <72753059+Mosbah-El-Ajmi@users.noreply.github.com>
2022-12-14 18:34:02 +0100

    get price meal, participation, sub/unsub

--- projet/meal.py
+++ projet/meal.py
@@ -4,10 +4,8 @@ class Meal:
     def __init__(self, Description, Date, Type, cooker):
         """"
             this function allows you to create a meal object
-
             PRE: Description , Date , type of cook , cooker who is the responsible of the meal and the list of participants
             POST: create a meal object
-
         """
         self._description = Description
         self.date = Date
@@ -20,17 +18,19 @@ class Meal:
              this function is aimed to return a textual description of the meal
              PRE: an meal object
              POST: a textual representation
-
         """
         return self._description
 
     def getTotalPrice(self,listofIngredients):
         """
             this function allows you to calculate the amount of a meal based of differents ingredients used
-
              PRE: a meal object and a list of ingredient objects
              POST: a textual representation of the amount of the meal
         """
+        total_price = 0
+        for ingredient in listofIngredients:
+            total_price += ingredient.price
+        return total_price
 
     def subscribe(self, user):
         """
@@ -38,6 +38,7 @@ class Meal:
         PRE: a meal object and a user object
         POST: a new list of participants
         """
+        self.participants.append(user.username)
 
     def unsubscribe(self,user):
         """
@@ -45,6 +46,7 @@ class Meal:
             PRE: a meal object and a user object
             POST: a new list of participants
         """
+        self.participants.remove(user.username)
 
 
     def changeCooker(self,user):
@@ -52,25 +54,21 @@ class Meal:
         this function allows you to change the meal's cooker
         PRE: a meal object and a user object
         POST: a new value for cooker attribut
-
         """
+        self.cooker = user
 
     def getParticipants(self):
         """
             this function allows you to display all the participants of a meal
             PRE: a meal object
             POST: a textual representation of participants
-
         """
+        return self.participants
 
     def getCooker(self):
         """
             this function allows you to display the responsible of a meal
             PRE: a meal object
             POST: a textual representation of the cooker
-
         """
-
-
-
-    pass
+        return f"the cooker is {self.cooker}"
