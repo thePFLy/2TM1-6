72baa2ae69fbfcc2eb57a505300df3bd77c3d058
Simon <HE202071@students.ephec.be>
2022-12-15 12:48:34 +0100

    pre spécifications

--- projet/user.py
+++ projet/user.py
@@ -7,7 +7,8 @@ path_database = "database/users.csv"
 
 def read_file(path: str):
     """this function allows you to read a file and append each line as user
-    PRE: receive a path already created (str)
+    PRE:
+    :path, a path already created (string)
     POST: returns a list that contains the user objects from the lines of the file as parameter
     """
     out = []
@@ -21,7 +22,9 @@ def read_file(path: str):
 def add_user_database(new_user, users_list):
     """this function allows you to add User (username hashed_password, my_salt) on database (csv file)
 
-    PRE: receive a User object and a list empty or not
+    PRE:
+    :new_user, a User object
+    :users_list, a list of users empty or not
     POST: append the user object to the list if the name of the object is not already in this list
     """
     for user in users_list:
@@ -33,7 +36,9 @@ def add_user_database(new_user, users_list):
 def delete_user_database(old_user, users_list):
     """this function allows you to delete User from a list
 
-    PRE: receive a User object and a list empty or not
+    PRE:
+    :old_user, an old User object (already in list)
+    :users_list, a list empty or not
     POST: delete the object if the name of the object is already in list
     """
     for user in users_list:
@@ -43,7 +48,9 @@ def delete_user_database(old_user, users_list):
 
 def write_file(path, users_list):
     """this function allows you to write objects (username hashed_password, my_salt) from list to file
-    PRE: receive a path already created of file  and user list empty or not
+    PRE:
+    :path, a path already created of file
+    :users_list, user list empty or not
     POST: write from a list the users object (username hashed_password, my_salt) for each lines of the file
     """
     tmp = ""
@@ -60,8 +67,10 @@ class Users:
 
     def __init__(self, username: str, hashed_password: str = None, my_salt: str = None):
         """ this function allows us to create a user based on his username and his password
-        PRE: receive username (str), a hashed password (str set to 0 by default) and a salt
-        (str set to 0 by default)
+        PRE:
+        :username, username (string)
+        :hashed_password, a hashed password (bytes set to None by default)
+        :my_salt, a salt (bytes)
         POST: a user object created
         """
         self._username = username
@@ -77,7 +86,8 @@ class Users:
 
     def is_correct_password(self, password: str):
         """ this function allows you to verify a password (str) correspond to the hashed password of user object
-        PRE: receive password (str len > 0)
+        PRE:
+        :password, password (string len > 0)
         POST: return True if the password correspond to the hashed password of user object and False if not
         """
         return hashlib.pbkdf2_hmac('sha256', password.encode(), self._my_salt, 100000) == self._hashed_password
@@ -85,7 +95,9 @@ class Users:
     def change_password(self, new_password: str, old_password: str = None):
         """this function allows you to change the password of a User object
 
-        PRE: receive a new password (str len >0) for the User object and the old password (str len >0)
+        PRE:
+        :new_password, a new password (str len >0) for the User object
+        :old_password, an old password (str len >0) already created or which don't exist for the User object
         POST: Change the hash and salt of user object if old password correspond or is not defined
         """
         if old_password is not None and not self.is_correct_password(old_password):
