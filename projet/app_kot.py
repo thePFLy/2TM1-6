5b4f44d244928cbde4037aeb81608b0602fcbcf7
PFLY <HE202071@students.ephec.be>
2022-12-17 20:29:25 +0100

    connect fini (manque connexion) + debut utilisateur connecté

--- projet/app_kot.py
+++ projet/app_kot.py
@@ -8,13 +8,31 @@ path_database = "database/users.csv"
 user_list = userclass.read_file(path_database)
 
 
+def user_connected():
+    print("-----Welcome on dining kot manager-----")
+    choice_task = int(input(
+        "Type:\n  1 see the schedule.\n  2 register for the meal of the day.\n  3 unsubscribe to meal of the day.\n 4 see invoice.\n 5 change password.\n 6 Sign out.\n 7 Sign out.\n"))
+    while choice_task != 1 and choice_task != 2 and choice_task != 3:
+        choice_task = int(input(
+            "Type:\n  1 log in.\n  2 register.\n  3 exit the program\n"))
+    if choice_task == 1:
+        connection()
+    elif choice_task == 2:
+        registration()
+    elif choice_task == 3:
+        delete_user()
+    elif choice_task == 4:
+        exit()
+
+
 def connection():
     print("------ login-----")
     user_connexion = str(input("Type your username\n"))
-    user_connexion_pwd = str(getpass("Type your password\n"))
+    user_connexion_pwd = str(input("Type your password\n"))
     for user in user_list:
         if user.username == user_connexion and user.is_correct_password(user_connexion_pwd):
             print("connected")
+            user_connected()
         else:
             print("invalid pwd or user")
             introduction()
@@ -23,9 +41,6 @@ def connection():
 def registration():
     print("------registration-----")
     user_registration = str(input("Type a username\n"))
-    for letter in user_registration:
-        if letter == "," or " ":
-            print("username cannot consist of commas or spaces")
     user_registration_pwd = str(input("Type a password\n"))
     user_registration_pwd_confirmation = str(input("Retype the password to confirm\n"))
     if user_registration_pwd == user_registration_pwd_confirmation:
@@ -33,6 +48,8 @@ def registration():
         tmp_user_registration.change_password(user_registration_pwd)
         userclass.add_user_database(tmp_user_registration, user_list)
         userclass.write_file(path_database, user_list)
+    else:
+        print("you did not enter the same password")
 
 
 def delete_user():
@@ -43,8 +60,7 @@ def delete_user():
 
 
 def introduction():
-    print(user_list)
-    print("-----Welcome on the kot dining manager-----")
+    print("-----Welcome on user connexion-----")
     introduction_connexion = int(input(
         "Type:\n  1 log in.\n  2 register.\n  3 delete account.\n 4 exit the program.\n"))
     while introduction_connexion != 1 and introduction_connexion != 2 and introduction_connexion != 3:
@@ -53,10 +69,7 @@ def introduction():
     if introduction_connexion == 1:
         connection()
     elif introduction_connexion == 2:
-        try:
-            registration()
-        except:
-            print("you did not enter the same password")
+        registration()
     elif introduction_connexion == 3:
         delete_user()
     elif introduction_connexion == 4:
