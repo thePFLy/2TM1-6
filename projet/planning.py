80b8127771d221c1a77fbfa5a4e0b23cf58bf8ac
Mosbah-El-Ajmi <72753059+Mosbah-El-Ajmi@users.noreply.github.com>
2022-12-18 11:26:59 +0100

    Traduction + change name of days

--- projet/planning.py
+++ projet/planning.py
@@ -4,9 +4,9 @@ from datetime import datetime, date, timedelta
 import logging
 
 
-def jourJ():
+def DDay():
     try:
-        with open('jourJ.csv') as csvfile:
+        with open('database/DDay.csv') as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
                 return row["Date"]
@@ -17,121 +17,113 @@ def jourJ():
 
 
 
-
-
-
-
-def newJourJ():
+def newDDay():
     newdate = datetime.today()
     formatted_date = newdate.strftime('%d-%m-%Y')
     try:
-        with open('jourJ.csv', 'w') as csvfile:
+        with open('database/DDay.csv', 'w') as csvfile:
             fieldnames = ['Date']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow({'Date': "Date"})
             writer.writerow({'Date': formatted_date})
     except OSError as e:
-            print(type(e), e)
+        print(type(e), e)
 
 
 
-
-
-def changement_planning(lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche):
-
+def change_planning(day1,day2,day3,day4,day5,day6,day7):
     try:
-        with open('planning.csv', 'w') as csvfile:
-            fieldnames = ['lundi', 'mardi', 'mercredi', 'jeudi', 'vendredi', 'samedi', 'dimanche']
+        with open('database/planning.csv', 'w') as csvfile:
+            fieldnames = ['Day 1', 'Day 2', 'Day 3', 'Day 4', 'Day 5', 'Day 6', 'Day 7']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
             writer.writerow(
-                {'lundi': 'lundi', 'mardi': 'mardi', 'mercredi': 'mercredi', 'jeudi': 'jeudi', 'vendredi': 'vendredi',
-                 'samedi': 'samedi', 'dimanche': 'dimanche'})
-            writer.writerow({'lundi': lundi, 'mardi': mardi, 'mercredi': mercredi, 'jeudi': jeudi, 'vendredi': vendredi,
-                             'samedi': samedi, 'dimanche': dimanche})
+                {'Day 1': 'Day 1', 'Day 2': 'Day 2', 'Day 3': 'Day 3', 'Day 4': 'Day 4', 'Day 5': 'Day 5',
+                 'Day 6': 'Day 6', 'Day 7': 'Day 7'})
+            writer.writerow({'Day 1': day1, 'Day 2': day2, 'Day 3': day3, 'Day 4': day4, 'Day 5': day5,
+                             'Day 6': day6, 'Day 7': day7})
     except OSError as e:
-            print(type(e), e)
+        print(type(e), e)
 
 
 
 
-
-def planning(etudiants):
+def planning(students):
     today = datetime.today()
-    ecart = today - jourJ
-    if ecart.days >= 7:
-        newJourJ()
-        print("Un changement dans le planning a eu lieu")
-        if len(etudiants) >= 7:
-            # Choisir 7 étudiants au hasard
-            etudiants_choisis = random.sample(etudiants, 7)
-            # Mélanger les étudiants choisis
-            random.shuffle(etudiants_choisis)
+    difference = today - DDay
+    if difference.days >= 7:
+        newDDay()
+        print("A change in the schedule has taken place")
+        if len(students) >= 7:
+            # Choose 7 random students
+            selected_students = random.sample(students, 7)
+            # Mixing selected students
+            random.shuffle(selected_students)
         else:
-            etudiants_choisis = etudiants
+            selected_students = students
 
-        # Répartir les étudiants sur les différents jours de la semaine
-        if len(etudiants_choisis) == 1:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[0]
-            mercredi = etudiants_choisis[0]
-            jeudi = etudiants_choisis[0]
-            vendredi = etudiants_choisis[0]
-            samedi = etudiants_choisis[0]
-            dimanche = etudiants_choisis[0]
-        if len(etudiants_choisis) == 2:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[1]
-            mercredi = etudiants_choisis[0]
-            jeudi = etudiants_choisis[1]
-            vendredi = etudiants_choisis[0]
-            samedi = etudiants_choisis[1]
-            dimanche = etudiants_choisis[0]
-        if len(etudiants_choisis) == 3:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[1]
-            mercredi = etudiants_choisis[2]
-            jeudi = etudiants_choisis[0]
-            vendredi = etudiants_choisis[1]
-            samedi = etudiants_choisis[2]
-            dimanche = etudiants_choisis[0]
-        if len(etudiants_choisis) == 4:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[1]
-            mercredi = etudiants_choisis[2]
-            jeudi = etudiants_choisis[3]
-            vendredi = etudiants_choisis[0]
-            samedi = etudiants_choisis[1]
-            dimanche = etudiants_choisis[2]
-        if len(etudiants_choisis) == 5:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[1]
-            mercredi = etudiants_choisis[2]
-            jeudi = etudiants_choisis[3]
-            vendredi = etudiants_choisis[4]
-            samedi = etudiants_choisis[0]
-            dimanche = etudiants_choisis[1]
-        if len(etudiants_choisis) == 6:
-            lundi = etudiants_choisis[1]
-            mardi = etudiants_choisis[0]
-            mercredi = etudiants_choisis[3]
-            jeudi = etudiants_choisis[2]
-            vendredi = etudiants_choisis[5]
-            samedi = etudiants_choisis[4]
-            dimanche = etudiants_choisis[0]
-        if len(etudiants_choisis) > 6:
-            lundi = etudiants_choisis[0]
-            mardi = etudiants_choisis[1]
-            mercredi = etudiants_choisis[2]
-            jeudi = etudiants_choisis[3]
-            vendredi = etudiants_choisis[4]
-            samedi = etudiants_choisis[5]
-            dimanche = etudiants_choisis[6]
-        changement_planning(lundi, mardi, mercredi, jeudi, vendredi, samedi, dimanche)
+        # Spreading the students over the different days of the week
+        if len(selected_students) == 1:
+            day1 = selected_students[0]
+            day2 = selected_students[0]
+            day3 = selected_students[0]
+            day4 = selected_students[0]
+            day5 = selected_students[0]
+            day6 = selected_students[0]
+            day7 = selected_students[0]
+        if len(selected_students) == 2:
+            day1 = selected_students[0]
+            day2 = selected_students[1]
+            day3 = selected_students[0]
+            day4 = selected_students[1]
+            day5 = selected_students[0]
+            day6 = selected_students[1]
+            day7 = selected_students[0]
+        if len(selected_students) == 3:
+            day1 = selected_students[0]
+            day2 = selected_students[1]
+            day3 = selected_students[2]
+            day4 = selected_students[0]
+            day5 = selected_students[1]
+            day6 = selected_students[2]
+            day7 = selected_students[0]
+        if len(selected_students) == 4:
+            day1 = selected_students[0]
+            day2 = selected_students[1]
+            day3 = selected_students[2]
+            day4 = selected_students[3]
+            day5 = selected_students[0]
+            day6 = selected_students[1]
+            day7 = selected_students[2]
+        if len(selected_students) == 5:
+            day1 = selected_students[0]
+            day2 = selected_students[1]
+            day3 = selected_students[2]
+            day4 = selected_students[3]
+            day5 = selected_students[4]
+            day6 = selected_students[0]
+            day7 = selected_students[1]
+        if len(selected_students) == 6:
+            day1 = selected_students[1]
+            day2 = selected_students[0]
+            day3 = selected_students[3]
+            day4 = selected_students[2]
+            day5 = selected_students[5]
+            day6 = selected_students[4]
+            day7 = selected_students[0]
+        if len(selected_students) > 6:
+            day1 = selected_students[0]
+            day2 = selected_students[1]
+            day3 = selected_students[2]
+            day4 = selected_students[3]
+            day5 = selected_students[4]
+            day6 = selected_students[5]
+            day7 = selected_students[6]
+        change_planning(day1,day2,day3,day4,day5,day6,day7)
     else:
         pass
 
 
-jourJ = datetime.strptime(jourJ(), "%d-%m-%Y")
+DDay = datetime.strptime(DDay(), "%d-%m-%Y")
 
 # test
 etudiants = ["Etudiant 1", "Etudiant 2", "Etudiant 3", "Etudiant 4", "Etudiant 5"]
