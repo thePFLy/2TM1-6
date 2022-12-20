d4db7004371244ce13843c0effd2e5f984dbbaf7
Mosbah-El-Ajmi <72753059+Mosbah-El-Ajmi@users.noreply.github.com>
2022-12-20 17:16:37 +0100

    Create class_plan.py

---
+++ projet/class_plan.py
@@ -0,0 +1,7 @@
+class Planning:
+    """
+        this class represent a day of the planning
+    """
+    def __init__(self, username: str, date):
+        self.username = username
+        self.date = date
