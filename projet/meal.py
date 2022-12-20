6ea97ef60becb91162684478c5347aa5f8880715
PFLY <HE202071@students.ephec.be>
2022-12-20 11:15:10 +0100

    specifications PRE POST Meal

--- projet/meal.py
+++ projet/meal.py
@@ -3,9 +3,14 @@ class Meal:
     """ class meal """
     def __init__(self, description, date, cooker, price_by_user: int, participants=None):
         """"
-            this function allows you to create a meal object
-            PRE: a str Description, Date, cooker who is the responsible for the meal and the list of participants
-            POST: create a meal object
+        this function allows you to create a meal object
+        PRE: a str Description, Date, cooker who is the responsible for the meal and the list of participants
+        :description, string with description of the meal
+        :date, string date of the meal
+        :cooker, string username of cooker (from Users)
+        :price_by_user, int cost of meal
+        :participants, list of participants
+        POST: create a meal object
         """
         if participants is None:
             participants = []
@@ -20,18 +25,19 @@ class Meal:
 
     def get_description(self):
         """
-             this function is aimed to return a textual description of the meal
-             PRE: a meal object
-             POST: a textual representation
+        this function is aimed to return a textual description of the meal
+        PRE: a meal object
+        POST: a textual representation
         """
         return self._description
 
     @staticmethod
     def get_total_price(list_ingredients):
         """
-            this function allows you to calculate the amount of a meal based of different ingredients used
-             PRE: a meal object and a list of ingredient objects
-             POST: a textual representation of the amount of the meal
+        this function allows you to calculate the amount of a meal based of different ingredients used
+        PRE:
+        :list_ingredients, list of ingredients objects
+        POST: a textual representation of the amount of the meal
         """
         total_price = 0
         for ingredient in list_ingredients:
@@ -41,17 +47,19 @@ class Meal:
     def subscribe(self, user):
         """
         this function allows a user to take part of a meal
-        PRE: a meal object and a user object
-        POST: add a new person in the list of participants
+        PRE:
+        :user, a User object
+        POST: add a new username in the list of participants
         """
 
         self.participants.append(user.username)
 
     def unsubscribe(self, user):
         """
-            this function allows a user to retire himself for a meal
-            PRE: a meal object and a user object
-            POST: remove a person from list of participants
+        this function allows a user to retire himself for a meal
+        PRE:
+        :user, a User object
+        POST: remove a person from list of participants
         """
         self.participants.remove(user.username)
 
@@ -59,22 +67,23 @@ class Meal:
         """
         this function allows you to change the meal's cooker
         PRE: a meal object and a user object
+        :user a User object
         POST: change the cooker name in cooker attribute of meal object
         """
         self.cooker = user
 
     def get_participants(self):
         """
-            this function allows you to display all the participants of a meal
-            PRE: a meal object
-            POST: a textual representation of participants
+        this function allows you to display all the participants of a meal
+        PRE: a meal object
+        POST: a textual representation of participants
         """
         return self.participants
 
     def get_cooker(self):
         """
-            this function allows you to display the responsible for a meal
-            PRE: a meal object
-            POST: a textual representation of the cooker
+        this function allows you to display the responsible for a meal
+        PRE: a meal object
+        POST: a textual representation of the cooker
         """
         return f"the cooker is {self.cooker}"
